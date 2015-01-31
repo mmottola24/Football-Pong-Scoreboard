9228cb75e89b971b63f10cb8883c5f29e7cbebef
Michael Mottola <mikemottola@gmail.com>
2015-01-31 01:04:42 -0500

    Fixed issue when newest version of express.
    Removed app.configure because in express v4 it is removed.

--- server.js
+++ server.js
@@ -9,9 +9,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.set("view options", { layout: false });
 
-app.configure(function() {
-  app.use(express.static(__dirname + '/public'));
-});
+app.use(express.static(__dirname + '/public'));
 
 server.listen(port);
 
